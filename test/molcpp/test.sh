d21b08be31f5ec42982e53183984187b9b4179c2
Roy Kid <lijichen@gmail.com>
2025-04-10 20:03:08 +0000

    reset version

--- test/molcpp/test.sh
+++ test/molcpp/test.sh
@@ -6,8 +6,12 @@ set -e
 source dev-container-features-test-lib
 
 # test if default shell is zsh
-check "cmake" cmake  --version
-check "clangd" clangd --version
+check "test_zsh_default" echo $SHELL | grep -q zsh
+check "test_cmake" cmake --version
+check "test_ninja" ninja --version
+check "test_clangd" clangd-20 --version
+check "test_clang" clang++-20 --version
+check "test_default_c++" c++ --version
 
 # Report result
 reportResults
