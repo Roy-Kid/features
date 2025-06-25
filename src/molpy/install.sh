787e8749ea50fef35ba632560278a2a776e7a434
Roy Kid <lijichen365@gmail.com>
2025-06-25 18:36:42 +0200

    fix bug

--- src/molpy/install.sh
+++ src/molpy/install.sh
@@ -5,6 +5,24 @@
 
 set -e
 
+# Color output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+echo_info() {
+    echo -e "${GREEN}[INFO]${NC} $1"
+}
+
+echo_warn() {
+    echo -e "${YELLOW}[WARN]${NC} $1"
+}
+
+echo_error() {
+    echo -e "${RED}[ERROR]${NC} $1"
+}
+
 # Function to install packages with conda
 install_conda_packages() {
     local packages="$1"
